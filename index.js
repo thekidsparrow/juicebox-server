18a4b367bd30a16aa2effafd488d894592e8468f
thekidsparrow <87579352+thekidsparrow@users.noreply.github.com>
2022-09-15 14:51:58 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
