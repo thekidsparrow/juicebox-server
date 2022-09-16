7db0b13a7bf8af2d883ee8f19f2f84c49036c43c
thekidsparrow <87579352+thekidsparrow@users.noreply.github.com>
2022-09-16 08:33:55 -0700

    Working on Middleware and Routes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
 });
 
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
